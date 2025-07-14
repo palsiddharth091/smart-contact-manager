1a3960d26b972f3835f81718136fc1782f0d4494
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2025-07-14 19:33:12 +0530

    Configuring Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/main/resources/templates/**/*.html',
+    './src/input.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'selector'
+}

4f32f7d56819b8447daeb66a8840bd7695e68afb
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2025-07-14 19:34:36 +0530

    Created Test Controller for Application Healthcheck

--- src/main/java/com/scm/scm20/Application.java
+++ src/main/java/com/scm/scm20/Application.java
@@ -2,8 +2,10 @@ package com.scm.scm20;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.scm.*")
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/scm/test/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.scm.test.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/hello")
+    public String greet() { // This method when hit will throw error because it will look for html or jsp with the name hello world
+        System.out.println("sid");
+        return "Hello World";
+    }
+
+    @GetMapping("/helloTemplate")
+    public String greetTemplate(Model model) {
+        System.out.println("sid");
+        model.addAttribute("name", "Siddharth");
+        model.addAttribute("title", "Testing");
+        return "helloTemplate";
+    }
+}

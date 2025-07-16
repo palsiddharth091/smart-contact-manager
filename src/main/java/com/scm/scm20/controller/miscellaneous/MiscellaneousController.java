42b04859935561111899bfb9cd98868b8091a7d2
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2025-07-16 19:58:05 +0530

    Changing redirection of HTML page as response in controller class

--- src/main/java/com/scm/scm20/controller/miscellaneous/MiscellaneousController.java
+++ src/main/java/com/scm/scm20/controller/miscellaneous/MiscellaneousController.java
@@ -1,9 +1,7 @@
 package com.scm.scm20.controller.miscellaneous;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -12,13 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MiscellaneousController {
 
 @GetMapping("/about")
-public String about() {
-    return "about";
+public String about(Model model) {
+    model.addAttribute("title", "About");
+    return "root/about/about";
 }
 
 @GetMapping("/services")
 public String services() {
-    return "services";
+    return "root/services/services";
 }
 
     

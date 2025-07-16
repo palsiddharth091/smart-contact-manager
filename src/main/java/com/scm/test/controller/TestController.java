6ba12d4effe937ac238e2c5a3aae4340c3ad94d9
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2025-07-16 20:42:50 +0530

    Removing unused imports

--- src/main/java/com/scm/test/controller/TestController.java
+++ src/main/java/com/scm/test/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.scm.test.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;

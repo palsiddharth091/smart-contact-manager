1c9fc0d37b6115a873a5237f0fcbc7464bade561
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2025-08-23 12:23:44 +0530

    Adding userform entity to encapsulate form data

---
+++ src/main/java/com/scm/scm20/forms/UserForm.java
@@ -0,0 +1,23 @@
+package com.scm.scm20.forms;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder    
+public class UserForm {
+    public String userName;
+    public String email;
+    public String password;
+    public String confirmPassword;
+    public String phoneNumber;
+    public String about;
+}

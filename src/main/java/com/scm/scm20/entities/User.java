21b4a293d04bd8909ca706d7fc8843bd06bedd2e
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2025-08-26 19:01:09 +0530

    Adding is Deleted column and changing datatype of few variables from Boolean to boolean

--- src/main/java/com/scm/scm20/entities/User.java
+++ src/main/java/com/scm/scm20/entities/User.java
@@ -3,12 +3,12 @@ package com.scm.scm20.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.Hibernate;
 import org.hibernate.annotations.ColumnDefault;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
@@ -34,6 +34,7 @@ import io.micrometer.common.lang.NonNull;
 @NoArgsConstructor
 @ToString
 @Builder
+@Entity
 public class User {
     
     @Id
@@ -41,7 +42,7 @@ public class User {
     @Column(name = "user_id")
     private Long userId;
     @Column(name = "user_name",length = 20)
-    private String username;
+    private String userName;
     private String name;
     @Column(nullable = false, unique = true)
     private String email;
@@ -54,19 +55,21 @@ public class User {
     private String phoneNumber;
 
     @ColumnDefault("false")
-    private String enabled; // Check if the user is enabled or not
+    private boolean enabled; // Check if the user is enabled or not
     @Column(name = "email_verified", columnDefinition = "default false")
-    private Boolean emailVerified; // Check if the email is verfifed or not
+    private boolean emailVerified; // Check if the email is verfifed or not
     @Column(name = "phone_verified", columnDefinition = "default false")
-    private Boolean phoneVerified; // Check if the phone is verfifed or not
-
+    private boolean phoneVerified; // Check if the phone is verfifed or not
+    
+    @Column(name = "is_deleted", columnDefinition = "default false")
+    private boolean isDeleted;
 
     @NonNull
     @Column(columnDefinition = "ENUM('SELF','GOOGLE','FACEBOOK')")
     @ColumnDefault("'SELF'")
     @Enumerated(EnumType.STRING)
     @Convert(converter = ProviderConverter.class) // When reading from the database, if the value is not a valid enum, it will not throw an exception. Instead, it will map to PROVIDER.SELF (or null, or any default you choose).
-    private PROVIDER provider; // To check how did the user sign up like Self, Google, Facebook etc. 
+    private PROVIDER provider = PROVIDER.SELF; // To check how did the user sign up like Self, Google, Facebook etc. 
 
     // Mapping one user to multiple contacts
     // The code establishes a one-to-many relationship between the User and `Contact` entities using JPA/Hibernate. Through the `@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)` annotation, a user can be associated with multiple contacts. The `mappedBy = "user"` parameter specifies that the `Contact` entity contains a `user` field that manages this relationship. `cascade = CascadeType.ALL` propagates all operations (like saving or deleting) from a User to their associated contacts. `fetch = FetchType.LAZY` when we fetch a user only user object is loaded and a proxy object of the contact class will be sent. It will fetch the contacts only when we hit a method something like that. `orphanRemoval = true` Automatically deletes contacts when the user is deleted.

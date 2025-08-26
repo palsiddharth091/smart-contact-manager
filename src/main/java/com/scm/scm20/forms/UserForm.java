ecc2f374c258efd1c68c46d9430fd9a115e4f94f
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2025-08-26 19:00:03 +0530

    Adding Name field in the UserForm model

--- src/main/java/com/scm/scm20/forms/UserForm.java
+++ src/main/java/com/scm/scm20/forms/UserForm.java
@@ -14,6 +14,7 @@ import lombok.ToString;
 @ToString
 @Builder    
 public class UserForm {
+    public String name;
     public String userName;
     public String email;
     public String password;

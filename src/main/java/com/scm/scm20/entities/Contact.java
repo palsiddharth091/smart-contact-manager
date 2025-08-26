7918c3f88ce6a62889ee4f994cd3192b07c89b05
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2025-08-26 19:01:49 +0530

    Adding entity annotation and FK Mappings column

--- src/main/java/com/scm/scm20/entities/Contact.java
+++ src/main/java/com/scm/scm20/entities/Contact.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,23 +26,29 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 @Builder
+@Entity
 public class Contact {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "contact_id")
     private Long contactId;
     private String name;
     private String email;
-    @Column(name="phone_number", length = 10)
+    @Column(name = "phone_number", length = 10)
     private String phoneNumber;
     private String address;
     private String picture;
     private String description;
     private boolean favorite = false;
-    
+
     // This is to store social media handles
-    @Column(name="social_media")
+    @Column(name = "social_media")
     @OneToMany(mappedBy = "contact", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
     List<SocialLink> socials = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
--- src/main/java/com/scm/scm20/entities/SocialLink.java
+++ src/main/java/com/scm/scm20/entities/SocialLink.java
@@ -1,9 +1,12 @@
 package com.scm.scm20.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,14 +21,19 @@ import lombok.ToString;
 @NoArgsConstructor
 @Builder
 @ToString
+@Entity
 @Table(name = "social_link")
 public class SocialLink {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "social_id")
     private Long socialId;
     private String link;
     private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "contact_id")
+    private Contact contact;
 
 }

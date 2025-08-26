7f06e09681757128a20520963a97966c2d4a5973
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2025-08-26 19:01:23 +0530

    Adding a class which will store constant for messages

---
+++ src/main/java/com/scm/scm20/constants/Messages.java
@@ -0,0 +1,9 @@
+package com.scm.scm20.constants;
+
+public class Messages {
+    
+    public static final String USER_ALREADY_EXISTS_BY_EMAIL = "User already exists with this email";
+    public static final String USER_ALREADY_EXISTS_BY_USERNAME = "User already exists with this username";
+    public static final String USER_ALREADY_EXISTS_BY_PHONE_NUMBER = "User already exists with this phone number";
+    public static final String ERROR_IN_DELETING = "User not found or already deleted.";
+}

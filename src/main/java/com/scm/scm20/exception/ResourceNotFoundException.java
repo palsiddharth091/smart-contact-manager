20dc2c5b63f650d9534ff2234f9c0bb904236886
palsiddharth091 <70902237+palsiddharth091@users.noreply.github.com>
2025-08-26 19:00:22 +0530

    Adding exception class incase resource not found

---
+++ src/main/java/com/scm/scm20/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.scm.scm20.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+    public ResourceNotFoundException(){
+        super("Resource Not Found Exception");
+    }
+}
